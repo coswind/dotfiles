467c46040e84f619e9408f432c91840755597032
Coswind Xia <wind@CoswinddeMacBook-Pro.local>
2025-12-28 19:28:36 +0800

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,15 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.max_fps = 60
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 0
+config.animation_fps = 1
+config.cursor_blink_rate = 0
+config.front_end = "OpenGL"
+
 config.initial_cols = 140
 config.initial_rows = 35
-config.window_background_opacity = 0.01
-config.macos_window_background_blur = 64
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = false
