8b0dc807694430af8ef06b1d6f2056165b92d3af
wind <cosx@capvision.com>
2025-12-29 15:28:10 +0800

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,8 +2,8 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.max_fps = 60
-config.window_background_opacity = 0.85
-config.macos_window_background_blur = 0
+config.window_background_opacity = 0.01
+config.macos_window_background_blur = 64
 config.animation_fps = 1
 config.cursor_blink_rate = 0
 config.front_end = "OpenGL"
@@ -16,7 +16,7 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
 config.color_scheme = "Catppuccin Frappe"
-config.font_size = 18
+config.font_size = 20
 config.font = wezterm.font_with_fallback({
   "Iosevka",
   "隶变-简",
